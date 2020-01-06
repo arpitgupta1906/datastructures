b5127ed3854f28331fb2d45eedd77bc19fb78806
arpitgupta1906 <gupta.25.iitj.ac.in>
2020-01-06 21:30:55 +0530

    added deletion and other trie functions

--- datastructures/Trie/implementation.cpp
+++ datastructures/Trie/implementation.cpp
@@ -48,6 +48,69 @@ bool search(struct TrieNode* root,string key){
   return (temp!=NULL && temp->isEndOfWord);
 }
 
+bool isEmpty(TrieNode* root){
+  for(int i=0;i<26;i++){
+    if(root->children[i])
+      return false;
+  }
+  return true;
+}
+
+struct TrieNode* remove(TrieNode* root,string key,int depth=0){
+  if(!root)
+    return NULL;
+
+  if(depth==key.size()){
+    if(root->isEndOfWord)
+      root->isEndOfWord=false;
+
+    if(isEmpty(root)){
+
+      delete(root);
+      root=NULL;
+    }
+    return root;
+  }
+
+  int index=key[depth]-'a';
+  root->children[index]=remove(root->children[index],key,depth+1);
+
+  if(isEmpty(root) && root->isEndOfWord==false)
+    {
+      delete(root);
+      root=NULL;
+    }
+    return root;
+}
+
+int wordCount(struct TrieNode* root){
+  int result=0;
+
+  if(root->isEndOfWord)
+    result++;
+
+  for(int i=0;i<26;i++){
+    if(root->children[i])
+      result+=wordCount(root->children[i]);
+  }
+
+    return result;
+}
+
+void display(struct TrieNode* root,char str[],int level=0){
+  if(root->isEndOfWord){
+    str[level]='\0';
+    cout<<str<<endl;
+  }
+
+  for(int i=0;i<26;i++){
+    if(root->children[i]){
+      str[level]=i+'a';
+      display(root->children[i],str,level+1);
+    }
+  }
+}
+
 int main(){
   string keys[] = {"the", "a", "there",
                     "answer", "any", "by",
@@ -56,14 +119,16 @@ int main(){
 
     struct TrieNode *root = getNode();
 
-    // Construct trie
+
     for (int i = 0; i < n; i++)
         insert(root, keys[i]);
 
-    // Search for different keys
-    search(root, "the")? cout << "Yes\n" :
-                         cout << "No\n";
-    search(root, "these")? cout << "Yes\n" :
-                           cout << "No\n";
-    return 0; 
+
+    search(root, "the")? cout << "Yes\n" : cout << "No\n";
+    search(root, "these")? cout << "Yes\n" :cout << "No\n";
+
+    remove(root,"the");
+    char str[20];
+    display(root,str);
+    return 0;
 }
